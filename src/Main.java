b9871a1914840c43a85fc001bf3e6c0fb2461944
Arwa Kamal <83993335+Arwakamal@users.noreply.github.com>
2022-06-18 13:28:11 +0200

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,27 @@
-import java.time.Month;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int Month ,year;
-        System.out.println("Enter the Month Number : ");
+        int Day ;
+        System.out.println("Enter your Number : ");
         Scanner scanner=new Scanner(System.in);
-        Month =scanner.nextInt();
-        System.out.println("Enter the year Number : ");
-        year=scanner.nextInt();
+        Day=scanner.nextInt();
 
-        String MonthName="",NumOfDays="";
-        switch (Month){
-
-            case 1: MonthName="January";
-                NumOfDays="31";
-                break;
-            case 2: MonthName="February";
-                if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
-                    NumOfDays= "29";
-                } else {
-                    NumOfDays = "28";
-                }
-                break;
-            case 3: MonthName="March";
-                NumOfDays="31";
-                break;
-            case 4: MonthName="April";
-                NumOfDays="30";
-                break;
-            case 5: MonthName="May";
-                NumOfDays="31";
-                break;
-            case 6: MonthName="June";
-                NumOfDays="30";
-                break;
-            case 7: MonthName="July";
-                NumOfDays="31";
-                break;
-            case 8: MonthName="August";
-                NumOfDays="31";
-                break;
-            case 9: MonthName="September";
-                NumOfDays="30";
-                break;
-            case 10: MonthName="October";
-                NumOfDays="31";
-                break;
-            case 11: MonthName="November";
-                NumOfDays="30";
-                break;
-            case 12: MonthName="December";
-                NumOfDays="31";
-                break;
-
-        }
-        System.out.println(MonthName+" "+ year+ " has " +NumOfDays+" Days.");
+        System.out.println(GetTheDayName(Day));
 
     }
-
+    public static String GetTheDayName(int Day){
+        String DayName="";
+        switch (Day){
+            case 1: DayName="Monday";break;
+            case 2: DayName="Tuesday";break;
+            case 3: DayName="Wednesday";break;
+            case 4: DayName="Thursday";break;
+            case 5: DayName="Friday";break;
+            case 6: DayName="Saturday";break;
+            case 7: DayName="Sunday";break;
+            default: DayName="Invalid day range";break;
+        }
+        return DayName;
+    }
 }
